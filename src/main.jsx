c9cd9beb442af81fa1340ca650f98c24a0b7e349
Taranjeet Singh <taranjeetsinghtanu2@gmail.com>
2024-10-20 19:02:50 +0200

    Deleted AuthContext and TaskContext, added AuthProvider to centralize data management in a single file.

--- src/main.jsx
+++ src/main.jsx
@@ -2,15 +2,12 @@ import { StrictMode } from 'react'
 import { createRoot } from 'react-dom/client'
 import App from './App.jsx'
 import './index.css'
-import AuthCotext from './context/AuthContext.jsx'
-import TaskContext from './context/TaskContext.jsx'
+import AuthProvider from './context/AuthProvider.jsx'
 
 createRoot(document.getElementById('root')).render(
-  <StrictMode>
-    <AuthCotext>
-      <TaskContext>
-         <App /> 
-      </TaskContext>
-    </AuthCotext>
+  <StrictMode>  
+    <AuthProvider>
+      <App />     
+    </AuthProvider> 
   </StrictMode>,
 )
