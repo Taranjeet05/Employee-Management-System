c4670128ad9d0b031dc085274cb0970f303584b8
Taranjeet Singh <taranjeetsinghtanu2@gmail.com>
2024-10-20 16:22:10 +0200

    Set up project structure with AuthContext and TaskContext, wrapped App component with both contexts for global state management. Fixed typo in AuthContext import.

---
+++ src/context/TaskContext.jsx
@@ -0,0 +1,14 @@
+//import React from 'react';
+import PropTypes from 'prop-types';  
+
+const TaskContext = ({ children }) => {
+  return (
+    <div>{children}</div>  
+  );
+};
+
+TaskContext.propTypes = {
+  children: PropTypes.node.isRequired,  
+};
+
+export default TaskContext;
--- src/main.jsx
+++ src/main.jsx
@@ -2,9 +2,15 @@ import { StrictMode } from 'react'
 import { createRoot } from 'react-dom/client'
 import App from './App.jsx'
 import './index.css'
+import AuthCotext from './context/AuthContext.jsx'
+import TaskContext from './context/TaskContext.jsx'
 
 createRoot(document.getElementById('root')).render(
   <StrictMode>
-    <App />
+    <AuthCotext>
+      <TaskContext>
+         <App /> 
+      </TaskContext>
+    </AuthCotext>
   </StrictMode>,
 )
